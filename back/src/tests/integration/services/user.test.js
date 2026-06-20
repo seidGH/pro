2b4e73ad5f1ff434a03ddf3a381982d3753e047f
seid <seyamen@gmail.com>
2026-06-20 19:16:20 +0300

    sss

--- back/src/tests/integration/services/user.test.js
+++ back/src/tests/integration/services/user.test.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { UserService } from '../../../src/services/userService.js';
+import { UserService } from '../../../../src/services/userService.js';
 
 const prisma = new PrismaClient();
 const userService = new UserService(prisma);
